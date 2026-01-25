ce232e6b2086f894a71275250327f7c8bfc5a636
Vidit Roy <viditroy2003@gmail.com>
2026-01-25 16:05:56 +0530

    Working clean backend setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,20 +11,20 @@ import { NotificationModule } from './notification/notification.module';
 import { PresenceModule } from './presence/presence.module';
 import { RateLimitModule } from './rate-limit/rate-limit.module';
 import { ChatGateway } from './chat/chat.gateway';
-import { TypeOrmModule } from '@nestjs/typeorm';
+//import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DB_HOST || 'localhost',
+    //   port: parseInt(process.env.DB_PORT || '5432'),
+    //   username: process.env.DB_USER
+    //   password: process.env.DB_PASS,
+    //   database: process.env.DB_NAME,
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
     AuthModule,
     UserModule,
     ConversationModule,
